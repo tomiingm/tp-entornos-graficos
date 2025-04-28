24edf21f18c128307da7b110f5b9ab2cc5c068fc
Francisco <fransbebobruno@gmail.com>
2025-04-28 12:49:01 -0300

    cambiando la estructura basica

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'vacantes-docentes');
-
-define('BASE_URL', 'http://localhost/tp-entornos-graficos/');
-
-session_start();
