ac68f9fff0a8d124cf032a2b72d4849763b098fd
Jeronimo <jeronatan@hotmail.com>
2025-08-13 11:00:02 -0300

    arreglos - 2

--- nuestro-proyecto-web/index.php
+++ nuestro-proyecto-web/index.php
@@ -8,9 +8,10 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Portal de Vacantes Docentes UTN</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link href="/css/estilos.css" rel="stylesheet">
+  <link href="css/estilos.css" rel="stylesheet">
+  <link rel="icon" href="/nuestro-proyecto-web/assets/images/utn.ico" type="image/x-icon">
 </head>
-<body class="d-flex flex-column min-vh-100">
+<body class="d-flex flex-column min-vh-100">  
 
 
   <!-- Navbar -->
@@ -47,21 +48,21 @@ session_start();
 
 
 
-  <div class="container d-flex flex-column align-items-center text-center">
+  <div class="container d-flex flex-column align-items-center text-center bg-white border rounded-4 shadow my-5 p-4">
 
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_FEqjavcxlrifqvl75bLKmY4my0fdwLqDmQ&s" alt="Logo Universidad" class="logo-facu" >
 
-    <h1 class="fw-bold mb-3">Bienvenidos</h1>
+    <h1 class="fw-bold mb-3">Portal de Vacantes</h1>
 
-    <p class="text-muted" style="max-width: 600px;">
+    <p class="text-muted fst-italic" style="max-width: 600px;">
       En nuestra facultad, reconocemos la importancia de gestionar de manera eficiente la selección y contratación de personal docente. Por ello, ponemos a disposición este portal de vacantes, diseñado para facilitar la publicación de oportunidades laborales, la recepción de postulaciones y la evaluación de candidatos. A través de una plataforma moderna, intuitiva y responsiva, buscamos optimizar los procesos de recursos humanos, garantizando transparencia, agilidad y accesibilidad para todos los interesados.
     </p>
 
     <!-- Botones -->
    <div class="d-flex justify-content-center gap-3 mt-4"> 
       <?php if (!isset($_SESSION["usuario_id"])): ?>
-        <a href="php/login.php" class="btn btn-primary px-4">Iniciar Sesión</a>
-        <a href="php/registro.php" class="btn btn-outline-dark px-4">Registrarse</a>
+        <a href="php/login.php" class="btn btn-success px-2 "><strong>Iniciar Sesión</strong></a>
+        <a href="php/registro.php" class="btn btn-outline-info px-3 text- "><strong>Registrarse</strong></a>
       <?php else: ?>
         <span class="align-self-center fw-bold">
           Hola, <?= htmlspecialchars($_SESSION["nombre"] . " " . $_SESSION["apellido"]) ?>
